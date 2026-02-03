28a55478367e1eabd3d5a1ccf75e4fd440b7f664
LIU CHIAWEI <doublecheap@gmail.com>
2026-02-03 13:41:20 +0900

    fix: theme provider

--- components/providers/theme-provider.tsx
+++ components/providers/theme-provider.tsx
@@ -6,6 +6,8 @@ import { type ReactNode } from "react";
 export function ThemeProvider({ children }: { children: ReactNode }) {
   return (
     <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
       enableSystem={true}
       disableTransitionOnChange={false}
     >
